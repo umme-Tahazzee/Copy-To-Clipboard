1aa87039b76d5de947c86c188bc6ad38ab7186ab
umme-Tahazzee <tahazzeekaria1@gmail.com>
2022-07-22 09:43:35 +0600

    feat: :sparkles:

---
+++ app.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('input')
+const btn = document.querySelector('button.btn')
+
+btn.onclick = function(){
+    input.select();
+    document.execCommand('copy');
+
+alert('copied')
+
+}
